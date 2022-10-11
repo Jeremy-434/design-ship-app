4b7c8a3d7c96f55195583dddbb712ec2fc359436
NIXON0220 <danielalvarezz495@gmail.com>
2022-10-10 23:49:26 -0500

    add Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html'],
+  content: [
+  './public/index.html', 
+  './public/plantillasweb/plantillas.html'
+],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Fredoka':['Fredoka','medium','sans-serif'],
+        'Fredoka2':['Fredoka','bold','sans-serif'],
+        'Fredoka3':['Fredoka','light','sans-serif'],
+        'Fredoka4':['Fredoka','regular','sans-serif'],
+        'Fredoka5':['Fredoka','semi-bold','sans-serif'],
+        },
+    },
   },
   plugins: [],
 }
