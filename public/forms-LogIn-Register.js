a7f2ee6060367c6636a69970ec764b005390ccac
jeremy <jeremy9451.lmd@gmail.com>
2022-10-17 22:26:23 -0500

    Add localStorage funtion

--- public/forms-LogIn-Register.js
+++ public/forms-LogIn-Register.js
@@ -1,9 +1,32 @@
 
+// FUNCION PARA CARGAR EL USUARIO EN EL NAVBAR
+const printUserInNavbar = document.querySelector("#userNavbar");
+const loadUser = () => {
+  if (localStorage.email != "") {
+    console.log("noundefined");
+    printUserInNavbar.classList.remove("hidden");
+  } else {
+    console.log("undefined");
+    printUserInNavbar.classList.add("hidden");
+  }
+}
+loadUser();
 
+// BOTON PARA DESLOGUEARSE
+const logOut = document.querySelector("#logOut");
+logOut.addEventListener("click", () => {
+  localStorage.name = "";
+  localStorage.user = "";
+  localStorage.password = "";
+  localStorage.email = "";
+  printUserInNavbar.classList.add("hidden");
+});
+
+// FUNCION PARA CARGAR EL MODAL DEL FORMULARIO DE REGISTRO
 const showRegister = () => {
-    const divRegister = document.getElementById('divRegister');
+  const divRegister = document.getElementById('divRegister');
 
-    divRegister.innerHTML = `<!-- FORMULARIO DE REGISTRO -->
+  divRegister.innerHTML = `<!-- FORMULARIO DE REGISTRO -->
     <div
         class="modal-containerRegster modalRegster overflow-y-auto overflow-x-auto fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center bg-black/50">
         <div class="relative p-4 w-full max-w-md h-full md:h-auto">
@@ -47,6 +70,7 @@ const showRegister = () => {
                                 <div id="warningPassword" class="text-rose-500 text-xs"></div>
                         </div>
                         <div class="my-4 lg:my-8">
+                        <div id="enterUser" class="text-green-500 text-lg"></div>
                             <p class="text-xs sm:text-sm">
                                 <i class="font-medium cursor-pointer text-sky-400">
 
@@ -65,65 +89,85 @@ const showRegister = () => {
             </div>
         </div>
     </div>`;
-    let cerrar = document.querySelectorAll(".closeRegster")[0];
-    let modal = document.querySelectorAll(".modalRegster")[0];
-    let modalC = document.querySelectorAll(".modal-containerRegster")[0];
-    cerrar.addEventListener("click", function () {
-        console.log('asd')
-        modal.classList.toggle("hidden");
-        modalC.style.opacity = "1";
-        modalC.style.visibility = "visible";
-    });
+  let cerrar = document.querySelectorAll(".closeRegster")[0];
+  let modal = document.querySelectorAll(".modalRegster")[0];
+  let modalC = document.querySelectorAll(".modal-containerRegster")[0];
+  cerrar.addEventListener("click", function () {
+    console.log('asd')
+    modal.classList.toggle("hidden");
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+  });
 
-    // VALIDACIONES DEL FORMULARIO
-    const form = document.querySelector("#form");
-    const firstNameFormRegister = document.querySelector("#firstNameFormRegister");
-    const lastNameFormRegister = document.querySelector("#lastNameFormRegister");
-    const emailFormRegister = document.querySelector("#emailFormRegister");
-    const userNameFormRegister = document.querySelector("#userNameFormRegister");
-    const passwordFormRegister = document.querySelector("#passwordFormRegister");
-    const warningFirstName = document.querySelector("#warningFirstName");
-    const warningLastName = document.querySelector("#warningLastName");
-    const warningEmail = document.querySelector("#warningEmail");
-    const warningUserName = document.querySelector("#warningUserName");
-    const warningPassword = document.querySelector("#warningPassword");
+  // VALIDACIONES DEL FORMULARIO
+  const form = document.querySelector("#form");
+  const firstNameFormRegister = document.querySelector("#firstNameFormRegister");
+  const lastNameFormRegister = document.querySelector("#lastNameFormRegister");
+  const emailFormRegister = document.querySelector("#emailFormRegister");
+  const userNameFormRegister = document.querySelector("#userNameFormRegister");
+  const passwordFormRegister = document.querySelector("#passwordFormRegister");
+  const warningFirstName = document.querySelector("#warningFirstName");
+  const warningLastName = document.querySelector("#warningLastName");
+  const warningEmail = document.querySelector("#warningEmail");
+  const warningUserName = document.querySelector("#warningUserName");
+  const warningPassword = document.querySelector("#warningPassword");
+  const enterUser = document.querySelector("#enterUser");
 
-    form.addEventListener("submit", event => {
-        event.preventDefault();
-        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    let enter = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-        if (firstNameFormRegister.value.length < 6) {
-            warningFirstName.innerHTML = "Please enter a valid first name.";
-        } else {
-            warningFirstName.innerHTML = "";
-        }
-        if (lastNameFormRegister.value.length < 6) {
-            warningLastName.innerHTML = "Please enter a valid last name";
-        } else {
-            warningLastName.innerHTML = "";
-        }
-        if (!regexEmail.test(emailFormRegister.value)) {
-            warningEmail.innerHTML = "Please enter a valid email address";
-        } else {
-            warningEmail.innerHTML = "";
-        }
-        if (userNameFormRegister.value.length < 6) {
-            warningUserName.innerHTML = "Please enter a valid user name";
-        } else {
-            warningUserName.innerHTML = "";
-        }
-        if (passwordFormRegister.value.length < 8) {
-            warningPassword.innerHTML = "Please enter a valid password";
-        } else {
-            warningPassword.innerHTML = "";
-        }
-    });
+    if (firstNameFormRegister.value.length < 6) {
+      warningFirstName.innerHTML = "Please enter a valid first name.";
+      enter = true;
+    } else {
+      warningFirstName.innerHTML = "";
+    }
+    if (lastNameFormRegister.value.length < 6) {
+      warningLastName.innerHTML = "Please enter a valid last name";
+      enter = true;
+    } else {
+      warningLastName.innerHTML = "";
+    }
+    if (!regexEmail.test(emailFormRegister.value)) {
+      warningEmail.innerHTML = "Please enter a valid email address";
+      enter = true;
+    } else {
+      warningEmail.innerHTML = "";
+    }
+    if (userNameFormRegister.value.length < 6) {
+      warningUserName.innerHTML = "Please enter a valid user name";
+      enter = true;
+    } else {
+      warningUserName.innerHTML = "";
+    }
+    if (passwordFormRegister.value.length < 8) {
+      warningPassword.innerHTML = "Please enter a valid password";
+      enter = true;
+    } else {
+      warningPassword.innerHTML = "";
+    }
+    if (!enter) {
+      localStorage.name = firstNameFormRegister.value + " " + lastNameFormRegister.value;
+      localStorage.user = userNameFormRegister.value;
+      localStorage.password = passwordFormRegister.value;
+      localStorage.email = emailFormRegister.value;
+      enterUser.innerHTML = "¡BIENVENIDO!"
+      setTimeout(() => { 
+      modal.classList.toggle("hidden")
+      loadUser() }, 1000)
+    }
+
+  });
+
 }
 
+// FUNCION PARA CARGAR EL MODAL DEL FORMULARIO DE INGRESAR
 const showLogin = () => {
-    const divLogin = document.getElementById('divLogin');
+  const divLogin = document.getElementById('divLogin');
 
-    divLogin.innerHTML = `<!-- FORMULARIO DE INGRESO -->
+  divLogin.innerHTML = `<!-- FORMULARIO DE INGRESO -->
     <div
         class="modal-container modal overflow-y-auto overflow-x-auto fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full flex justify-center items-center bg-black/50">
         <div class="relative p-4 w-full max-w-md h-full md:h-auto">
@@ -149,6 +193,7 @@ const showLogin = () => {
                                 <div id="warningPasswordLogin" class="text-rose-500 text-xs"></div>
                             </div>
                             <div class="my-4 lg:my-8">
+                                <div id="enterUser" class="text-green-500 text-lg"></div>
                                 <p class="text-xs sm:text-sm">
                                     <i class="font-medium cursor-pointer text-sky-400">
                                         I forgot my password.
@@ -166,39 +211,63 @@ const showLogin = () => {
             </div>
         </div>
     </div>`
-    let cerrar = document.querySelectorAll(".closeLogin")[0];
-    let modal = document.querySelectorAll(".modal")[0];
-    let modalC = document.querySelectorAll(".modal-container")[0];
-    cerrar.addEventListener("click", function () {
-        console.log('asd')
-        modal.classList.toggle("hidden");
-        modalC.style.opacity = "1";
-        modalC.style.visibility = "visible";
-    });
+  let cerrar = document.querySelectorAll(".closeLogin")[0];
+  let modal = document.querySelectorAll(".modal")[0];
+  let modalC = document.querySelectorAll(".modal-container")[0];
+  cerrar.addEventListener("click", function () {
+    console.log('asd')
+    modal.classList.toggle("hidden");
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+  });
 
-    const form = document.querySelector("#formLogin");
-    const emailFormLogin = document.querySelector("#emailFormLogin");
-    const passwordFormLogin = document.querySelector("#passwordFormLogin");
-    const warningEmail = document.querySelector("#warningEmailLogin");
-    const warningPassword = document.querySelector("#warningPasswordLogin");
+  const form = document.querySelector("#formLogin");
+  const emailFormLogin = document.querySelector("#emailFormLogin");
+  const passwordFormLogin = document.querySelector("#passwordFormLogin");
+  const warningEmail = document.querySelector("#warningEmailLogin");
+  const warningPassword = document.querySelector("#warningPasswordLogin");
+  const enterUser = document.querySelector("#enterUser");
 
-    form.addEventListener("submit", event => {
-        event.preventDefault();
-        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    let enter = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-        if (!regexEmail.test(emailFormLogin.value)) {
-            warningEmail.innerHTML = "Please enter a valid email address";
-        }else{
-            warningEmail.innerHTML = "";
-        }
-        if (passwordFormLogin.value.length < 8) {
-            warningPassword.innerHTML = "Please enter a valid password";
-        }else{
-            warningPassword.innerHTML = "";
-        }
-    });
+    if (!regexEmail.test(emailFormLogin.value)) {
+      warningEmail.innerHTML = "Please enter a valid email address";
+      enter = true;
+    } else {
+      warningEmail.innerHTML = "";
+    }
+    if (passwordFormLogin.value.length < 8) {
+      warningPassword.innerHTML = "Please enter a valid password";
+      enter = true;
+    } else {
+      warningPassword.innerHTML = "";
+    }
+    if (!enter) {
+      localStorage.password = passwordFormLogin.value;
+      localStorage.email = emailFormLogin.value;
+      enterUser.innerHTML = "¡BIENVENIDO!"
+      setTimeout(() => {
+      modal.classList.toggle("hidden") 
+      loadUser()
+    }, 1000)
+    }
+  });
 
 }
+// let toggleDarkMode = new Boolean();
+// const darkmode = () => {
+//   toggleDarkMode.value = document.documentElement.classList.toggle("dark");
+//   toggleDarkMode.value
+//     ? (localStorage.theme = "dark")
+//     : (localStorage.theme = "light");
+// }
+// Cargar Elemento en el DOM para ver el icono del usuario en el navbar
+// let user = new Boolean();
+
+
 
 // export const shows = {
 //     showLogin,
