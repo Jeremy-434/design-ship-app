03c858942c7d8ee63c08c1f7f539baf74c0d7c11
NIXON0220 <danielalvarezz495@gmail.com>
2022-10-19 01:47:42 -0500

    Add PDF in shoppingCart.html

--- public/shoppingCart/pdf.js
+++ public/shoppingCart/pdf.js
@@ -1,49 +1,67 @@
 // setInterval(() => window.document.form_reloj.reloj.value = new Date().toLocaleTimeString(), 1000);
 
-// import { obtenerProductos } from "C:\Users\mafni\OneDrive\Escritorio\Kuepa\design-ship-app\public\fetch.js";
-// import { obtenerProductos } from "/Users/mafni/OneDrive/Escritorio/Kuepa/design-ship-app/public/fetch.js";
-
-let generateInvoice = document.querySelector('#generateInvoice');
-generateInvoice.addEventListener('click', () => {
-    // let products = obtenerProductos;
-    // console.log(products);
-    console.log("hola");
-    // let colorText = document.getElementById('exampleColorInput').value;
-    // console.log(colorText);
-
-    // function hexToRgb(hex) {
-    //   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    //   return result ? {
-    //     r: parseInt(result[1], 16),
-    //     g: parseInt(result[2], 16),
-    //     b: parseInt(result[3], 16)
-    //   } : null;
-    // }
-    // rgbColorR = hexToRgb(colorText).r;
-    // rgbColorG = hexToRgb(colorText).g;
-    // rgbColorB = hexToRgb(colorText).b;
-
-    // var doc = new jsPDF();
-    // doc.setFont('courier');
-    // doc.setFontType('bolditalic');
-    // doc.setFontSize(30);
-    // doc.setTextColor(38, 4, 118);
-    // doc.text(20, 15, 'FACTURA');
-    // doc.setTextColor(rgbColorR, rgbColorG, rgbColorB);
-    // doc.setFontType('');
-    // doc.setFontSize(8);
-    // doc.text(20, 30, 'Fecha de la factura: ' + new Date());
-    // doc.setFontSize(12);
-    // doc.text(20, 40, 'Documento: ' + doocument);
-    // doc.text(20, 45, 'Nombre: ' + name);
-    // doc.text(20, 50, 'Apellido: ' + lastName);
-    // doc.text(20, 55, 'Motivo: ' + reason);
-    // doc.text(20, 60, 'Fecha de inicio: ' + firstDate);
-    // doc.text(20, 65, 'Fecha de fin: ' + endDate);
-    // doc.text(20, 70, '¿Aplica descuento? ' + radioChecked);
-    // doc.setTextColor(5, 172, 2);
-    // doc.setFontSize(14);
-    // doc.text(120, 80, 'Valor de la factura: ' + valorTotal);
-    // doc.save('factura-pdf');
+// console.log(" hOALASDAS" + obtenerProductos);
+const fetchApi = async () => {
+  try {
+    const res = await fetch('http://127.0.0.1:8000/api/productos')
+    // console.log(res);
+    const data = res.json();
+    // console.log(data);
+    let valorTotal = 0;
+    // await data.forEach(product => {
+    //   let price = product.precio;
+    //   valorTotal += parseInt(price);
+    // });
+    return { data, valorTotal };
+  } catch (error) {
+    console.log(error);
   }
-);
+}
+
+fetchApi();
+
+// totalPrice.innerHTML = valorTotal;
+let generateInvoice = document.querySelector('#generateInvoice');
+let colorText = document.getElementById('exampleColorInput');
+generateInvoice.addEventListener('click', () => {
+  // ver la data de la funcion fetchApi
+  console.log(fetchApi());
+
+  let colorIncoice = colorText.value;
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+  rgbColorR = hexToRgb(colorIncoice).r;
+  rgbColorG = hexToRgb(colorIncoice).g;
+  rgbColorB = hexToRgb(colorIncoice).b;
+
+  var doc = new jsPDF();
+  doc.setFont('courier');
+  doc.setFontType('bolditalic');
+  doc.setFontSize(30);
+  doc.setTextColor(38, 4, 118);
+  doc.text(20, 15, 'FACTURA');
+  doc.setTextColor(rgbColorR, rgbColorG, rgbColorB);
+  doc.setFontType('');
+  doc.setFontSize(8);
+  doc.text(20, 30, 'Fecha de la factura: ' + new Date());
+  doc.setFontSize(16);
+  doc.text(20, 40, 'PRODUCTOS:');
+  // data.forEach(product => {
+  //   doc.setFontSize(12);
+  //   doc.text(20, 40, 'Name: ' + product.name);
+  //   doc.text(20, 45, 'Email: ' + product.email);
+  // });
+  doc.setFontSize(12);
+  doc.text(20, 40, 'Name: ');
+  doc.text(20, 45, 'Email: ');
+  doc.setTextColor(5, 172, 2);
+  doc.setFontSize(14);
+  doc.text(120, 80, 'Valor de la factura: ' );
+  doc.save('factura-pdf');
+});
